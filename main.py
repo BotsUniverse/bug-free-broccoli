07dace3b21866e20af45712ff8c81fbfb71029fb
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-03-30 18:41:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     with user_client:
         while True:
             print("[INFO] starting to check uptime..")
-            edit_text = f"@{update_channel} Bot's Uptime Status.(Updated every 15 mins)\n\n"
+            edit_text = f"Bot's Uptime Status.(Updated every 15 mins)\n\n"
             for bot in bots:
                 print(f"[INFO] checking @{bot}")
                 snt = user_client.send_message(bot, '/start')
@@ -30,12 +30,12 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"@{bot} status: ❎\n\n"
+                    edit_text += f"⚡️ @{bot} status: ❎\n\n"
                     user_client.send_message(bot_owner,
-                                             f"@{bot} status: `Down`")
+                                             f"⚡️ @{bot} status: ❎")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"@{bot} status: ✅\n\n"
+                    edit_text += f"⚡️ @{bot} status: ✅\n\n"
                 user_client.read_history(bot)
 
             utc_now = datetime.datetime.utcnow()
